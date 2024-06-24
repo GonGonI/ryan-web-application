81ba425ec64edd48f42218561ceef43576168987
Ryan Kim <ryankim@Ryanui-MacBookAir.local>
2024-06-24 13:52:09 +0900

    First commit

---
+++ src/main/kotlin/com/application/web/ryan/RyanApplication.kt
@@ -0,0 +1,11 @@
+package com.application.web.ryan
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RyanApplication
+
+fun main(args: Array<String>) {
+	runApplication<RyanApplication>(*args)
+}
